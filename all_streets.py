da19e1380b5f6f04e572c5d0217da916d7d4f66a
Joanne <71201494+Joanneylin@users.noreply.github.com>
2022-12-07 10:19:21 -0800

    netbuffer links and nodes + remove sub-network on islands

--- all_streets.py
+++ all_streets.py
@@ -1,9 +1,4 @@
-
-
-from wsgiref.util import shift_path_info
 import geopandas as gpd
-from geopandas.tools import sjoin
-import pandas as pd
 from pathlib import Path
 import yaml
 
@@ -19,7 +14,7 @@ config = yaml.safe_load(open(file))
 # read in OSM files
 if config['download_osm']:
     print("start downloading osm")
-    osm_gdf = osm_extract.download_osm(config['boundary_file_path'], config['crs'])
+    osm_gdf = osm_extract.download_osm(config['boundary_file_path'], config['psrc_buffer_file_path'], config['crs'])
 
     print("saving extracted osm to directory")
     osm_gdf.to_file(Path(config['output_dir'])/'osm_extract_2285.shp')
@@ -30,23 +25,27 @@ else:
 
 
 # split downloaded osm network
-split_net = osm_extract.generate_network_topology(osm_gdf, config['highway_types'])
+network_gdf, nodes_gdf = osm_extract.generate_network_topology(osm_gdf, config['highway_types'])
 # add ij nodes
-network, all_nodes = osm_extract.ij_nodes(split_net)
+# network_gdf = osm_extract.ij_nodes(split_net, config['crs'])
 
-main_network = osm_extract.rm_sub_network(network, config['islands_file_path'], config['crs'])
+main_network, main_nodes = osm_extract.rm_sub_network(network_gdf, nodes_gdf, config['islands_file_path'])
 
+links, nodes = osm_extract.create_link_node(main_network, main_nodes, config['netbuffer_crs'])
 
 # save results
 
 # network before removing disconnected sub-networks
-# network.to_file(Path(config['output_dir'])/'all_split.shp')
+network_gdf.to_file(Path(config['output_dir'])/'all_split.shp')
 
 # final network
 main_network.to_file(Path(config['output_dir'])/'final_network.shp')
 
-# list of all nodes in the network
-all_nodes.to_file(Path(config['output_dir'])/'all_nodes.shp')
+# all nodes in the network
+main_nodes.to_file(Path(config['output_dir'])/'all_nodes.shp')
 
+# nodes and links for netbuffer
+links.to_csv(Path(config['output_dir'])/'links.csv', index=False)
+nodes.to_csv(Path(config['output_dir'])/'nodes.csv', index=False)
 
 
--- osm_extract.py
+++ osm_extract.py
@@ -11,8 +11,7 @@ import json
 from pathlib import Path
 import networkx as nx
 
-
-def download_osm(boundary_file, crs):
+def download_osm(boundary_file, psrc_buffer_file, crs):
     with open(boundary_file) as f:
         data = json.load(f)
 
@@ -21,6 +20,11 @@ def download_osm(boundary_file, crs):
     gdf = geopandas_osm.osm.query_osm('way', poly, recurse='down', tags='highway')
     gdf = gdf[gdf.type == 'LineString'][['highway', 'name', 'geometry', 'bridge', 'tunnel', 'oneway']]
     gdf = gdf.to_crs(crs)
+
+    # clip network with buffered psrc region
+    psrc_buffer = gpd.read_file(Path(psrc_buffer_file)).to_crs(crs)
+    gdf = gpd.clip(gdf, psrc_buffer)
+
     return gdf
 
 
@@ -55,7 +59,10 @@ def generate_network_topology(osm_df, highway_types):
     roads = roads.sort_values(by=['id'])
     roads['link_id'] = [j + 1 for j in range(len(roads))]
 
-    return roads
+    # add i,j nodes, then return network and all nodes
+    network_gdf, nodes_gdf = ij_nodes(roads)
+
+    return network_gdf, nodes_gdf
 
 
 def split_segments(gdf, link_letter, crs=None):
@@ -116,13 +123,20 @@ def ij_nodes(gdf):
                              'coords': nodes_dict.keys()})
     df_nodes['geom_x'] = df_nodes['coords'].apply(lambda x: x[0])
     df_nodes['geom_y'] = df_nodes['coords'].apply(lambda x: x[1])
-    gdf_nodes = gpd.GeoDataFrame(df_nodes, geometry=gpd.points_from_xy(df_nodes['geom_x'], df_nodes['geom_y'])).drop(
-        columns=['coords', 'geom_x', 'geom_y'])
+    nodes_gdf = gpd.GeoDataFrame(df_nodes, geometry=gpd.points_from_xy(df_nodes['geom_x'], df_nodes['geom_y'])).drop(
+        columns=['coords', 'geom_x', 'geom_y']).set_crs(gdf_ij.crs)
 
-    return gdf_ij, gdf_nodes
+    # node.csv for netbuffer
+    # nodes2 = nodes_gdf.copy().to_crs(4326)  # change coordinates to wgs84
+    # nodes2['x'] = nodes2['geometry'].apply(lambda x: x.coords[0][0])
+    # nodes2['y'] = nodes2['geometry'].apply(lambda x: x.coords[0][1])
+    # node_list = pd.DataFrame(nodes2.drop(columns='geometry').rename(columns={"node_id": "id"}))
+
+    return gdf_ij, nodes_gdf
+
 
 # to remove disconnected sub-networks that are not on islands
-def rm_sub_network(gdf, islands, crs):
+def rm_sub_network(gdf, nodes_gdf, islands):
     x_graph = nx.Graph()
     graph = nx.from_pandas_edgelist(gdf, 'i_node', 'j_node',
                                     ['id', 'geometry', 'highway', 'name', 'bridge', 'tunnel', 'oneway', 'road_id',
@@ -132,20 +146,62 @@ def rm_sub_network(gdf, islands, crs):
 
     # combine all edges other than the main network
     df_list = []
-    for i in range(1, len(sep_graph)):
+    for i in range(1, len(sep_graph)):  # not including main network
         df = nx.to_pandas_edgelist(sep_graph[i])
+        df['sub_net_id'] = i  # name each sub-network
         df_list.append(df)
     all_disconnected = pd.concat(df_list, ignore_index=True)
 
     # create a gdf out of the disconnected network and clip with island shp
-    gdf_disconnect = gpd.GeoDataFrame(all_disconnected, geometry='geometry').set_crs(crs)
+    gdf_disconnect = gpd.GeoDataFrame(all_disconnected, geometry='geometry').set_crs(gdf.crs)
 
     # keep only networks on islands: clip disconnected networks using islands
-    islands = gpd.read_file(Path(islands)).to_crs(crs)
-    island_network = gpd.clip(gdf_disconnect, islands)
+    islands = gpd.read_file(Path(islands)).to_crs(gdf.crs)
+    island_network = gpd.sjoin(gdf_disconnect, islands[['countyname', 'geoid20', 'island_id', 'geometry']],
+                               how="inner", op='within')
+    # keep largest network in islands
+    island_network = island_network.loc[island_network.groupby(['island_id'])['sub_net_id'].apply(lambda x: x == min(x))]
 
     # gdf of main network
     df_final_network = nx.to_pandas_edgelist(sep_graph[0]).append(island_network)
-    final_network = gpd.GeoDataFrame(df_final_network, geometry='geometry').set_crs(crs)
+    final_network = gpd.GeoDataFrame(df_final_network, geometry='geometry').set_crs(gdf.crs)
 
-    return final_network
+    # node gdf
+    final_nodes = nodes_gdf.copy()
+
+    # keep nodes in network
+    res = [int(i) for i in final_network['i_node'].tolist() + final_network['j_node'].tolist()]
+    final_nodes = final_nodes[final_nodes['node_id'].isin(res)]
+
+    return final_network, final_nodes
+
+
+# create data input for netbuffer: link and node
+def create_link_node(gdf, nodes_gdf, out_crs):
+
+    gdf_link = gdf[['link_id', 'i_node', 'j_node', 'oneway', 'geometry']].copy()
+
+    #  calculate the distance in miles
+    gdf_link['distance'] = gdf_link['geometry'].length / 5280
+
+    links = gdf_link[['i_node', 'j_node', 'distance']].copy()
+    links['from'] = links['i_node']
+    links['to'] = links['j_node']
+    links = links[['from', 'to', 'distance']]
+
+    # add link in the reversed direction
+    links_op = gdf_link[gdf_link['oneway'] != 'yes'][['i_node', 'j_node', 'distance']].copy()
+    links_op['from'] = links_op['j_node']
+    links_op['to'] = links_op['i_node']
+    links_op['distance'] = links_op['distance']
+    links = links.append(links_op[['from', 'to', 'distance']].copy())
+
+    # node.csv for netbuffer
+    nodes = nodes_gdf.copy().to_crs(out_crs)  # change coordinates to wgs84
+
+    nodes['x'] = nodes['geometry'].apply(lambda x: x.coords[0][0])
+    nodes['y'] = nodes['geometry'].apply(lambda x: x.coords[0][1])
+
+    nodes_list = pd.DataFrame(nodes.drop(columns='geometry').rename(columns={"node_id": "id"}))
+
+    return links, nodes_list
